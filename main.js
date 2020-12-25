74d043afd4ba30d23eb613e2ed8600696f682ee2
Steward <go778sgt@gmail.com>
2020-12-25 00:46:27 -0600

    updated 24/12/2020

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const container = document.getElementById("models");
-/*
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid_item";
-  };
-};*/
 
 function showRecent() {
+  for (c = 0; c < recent.length; c++) {
+    var model = recent[c];
+    let cell = document.createElement("div");
+    cell.innerHTML = `
+      <span>
+      <img src="${model.image}.png" alt="${model.name}"/>
+      <div class="model_info "><p>${model.name}</p></div>
+    </span>
+    `;
+    cell.className = "model_item";
+    container.appendChild(cell);
+  }
+}
+function showOthers() {
   var rows = Math.floor(recent.length / 2);
   container.style.setProperty('--grid-rows', rows + 1);
   container.style.setProperty('--grid-cols', 2);
@@ -19,7 +23,9 @@ function showRecent() {
     let cell = document.createElement("div");
     cell.innerHTML = `
       <div class="model_item">
-        <img src="${model.image}.png" alt="${model.name}"/>
+        <span>
+          <img src="${model.image}.png" alt="${model.name}"/>
+        </span>
         <p>${model.name}</p>
       </div>
     `;
@@ -27,3 +33,15 @@ function showRecent() {
   }
 }
 showRecent();
+$('.sidebarbtn').click(function(){
+  $('.sidebarbtn').toggleClass("click");
+  $('.sidebar').toggleClass("show");
+});
+$('.catbtn').click(function() {
+  $('.catshow').toggleClass("show");
+  $('.first').toggleClass("rotate");
+});
+$('.htubtn').click(function() {
+  $('.htushow').toggleClass("show");
+  $('.second').toggleClass("rotate");
+});
